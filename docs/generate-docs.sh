1ee981610d01f07dee0b426415cf68cc6463bd6e
cudy789 <knuts983@umn.edu>
2024-12-05 01:19:56 -0600

    Build docs bash ci fix

--- docs/generate-docs.sh
+++ docs/generate-docs.sh
@@ -15,10 +15,14 @@ fi
 
 echo "Using image rogueraptor7/$IMAGE_NAME:$IMAGE_TAG"
 
+echo "Killing any existing ampl containers"
+docker kill ampl-docs
+sleep 5
+
 echo "Pulling latest image"
 docker pull $ARCH rogueraptor7/$IMAGE_NAME:$IMAGE_TAG
 
-docker run --rm -h $IMAGE_NAME-$HOSTNAME --name ampl-tests --group-add sudo --group-add video --add-host $IMAGE_NAME-$HOSTNAME:127.0.0.1 --network host \
+docker run --rm -h $IMAGE_NAME-$HOSTNAME --name ampl-docs --group-add sudo --group-add video --add-host $IMAGE_NAME-$HOSTNAME:127.0.0.1 --network host \
   --user=$(id -u $USER):$(id -g $USER) \
   --volume="/etc/passwd:/etc/passwd:ro" \
   --volume="/etc/shadow:/etc/shadow:ro" \

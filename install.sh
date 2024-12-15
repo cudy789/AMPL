947a3634ce2c7065dbc3d9b98a1f29f11c594626
cudy789 <knuts983@umn.edu>
2024-12-15 14:13:58 -0600

    new docker and docker compose install steps

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ else
     echo "Docker is not installed. Proceeding with installation..."
 
     # Update package index
-    sudo apt update -y
+    sudo apt update
 
     # Install prerequisites
     sudo apt install -y \
@@ -23,19 +23,11 @@ else
         curl \
         software-properties-common
 
-    # Add Docker's official GPG key
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-    # Set up the stable repository
-    echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
     # Update package index again
-    sudo apt update -y
+    sudo apt update
 
     # Install Docker Engine
-    sudo apt install -y docker-ce docker-ce-cli containerd.io
+    sudo apt install -y docker.io
 
     # Enable and start Docker service
     sudo systemctl enable docker
@@ -52,8 +44,11 @@ if command_exists docker compose; then
 else
     echo "Docker Compose is not installed. Proceeding with installation..."
 
-    # Download the current stable release of Docker Compose
-    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    # Update package index
+    sudo apt update
+
+    # Install docker compose
+    sudo apt install docker-compose
 
     # Verify installation
     if command_exists docker compose; then

d5e6f679794656415d5a634e2ae3ae0b9a6465e3
cudy789 <knuts983@umn.edu>
2025-01-05 20:16:57 -0600

    tag images for sim

---
+++ scripts/dev-build-and-run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Corey Knutson, 12/2/2021
+
+# Do a grep exclude at the end to remove error messages of Corrupt JPEG data. Cause by a bug in OpenCV's libjpeg library
+# For more details... https://github.com/opencv/opencv/issues/9477
+bash scripts/run-common.sh "mkdir -p build && cd build && cmake .. && make -j4 && ./ampl;" 2>&1 | grep -v "Corrupt JPEG data"
